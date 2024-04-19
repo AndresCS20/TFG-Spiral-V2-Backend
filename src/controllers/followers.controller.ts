a4e95eda1aafe5b8681026d60785bcdf2cefb8d2
AndresCS20 <andrs20cs@gmail.com>
2024-04-19 23:18:55 +0200

    Terminada funcionalidad follows(following/followers)

--- src/controllers/followers.controller.ts
+++ src/controllers/followers.controller.ts
@@ -1,29 +1,51 @@
 import { handleHttp } from "@utils/error.handle";
 import { Request, Response } from "express";
-import { insertFollowSvc, deleteFollowSvc } from "@services/follows.services";
+import { insertFollowSvc, deleteFollowSvc, isFollowingUser } from "@services/follows.services";
 
-//TODO: Añadir status code y mensaje
 const followUser = async ({ body }: Request, res: Response) => {
-    
     try {
-        const userId = body.id;
-        const followId = body.followId;
-        const responseItem = await insertFollowSvc(userId, followId);
-        res.send(responseItem);
-      } catch (e) {
-        handleHttp(res, "ERROR_POST_USER", e);
+      const userId = body.userId;
+      const followId = body.followId;
+  
+      // Verificar si los usuarios ya se están siguiendo
+      const isFollowing: boolean = await isFollowingUser(userId, followId);
+  
+      if (isFollowing) {
+        res.status(400).send({ message: 'Los usuarios ya se están siguiendo' });
+        return;
       }
+  
+      const responseItem = await insertFollowSvc(userId, followId);
+      res.status(201).send(responseItem);
+    } catch (e) {
+      handleHttp(res, "ERROR_POST_USER", e);
+    }
   }
 
+// const followUser = async ({ body }: Request, res: Response) => {
+    
+//     try {
+//         const userId = body.userId;
+//         const followId = body.followId;
+//         const responseItem = await insertFollowSvc(userId, followId);
+//         res.status(201);
+//         res.send(responseItem);
+//       } catch (e) {
+//         handleHttp(res, "ERROR_POST_USER", e);
+//       }
+//   }
+
   
-  //TODO: Añadir status code y mensaje
 const unFollowUser = async ({ body }: Request, res: Response) => {
   
     try {
-        const userId = body.id;
+        console.log(body);
+        const userId = body.userId;
         const followId = body.followId;
         const response = await deleteFollowSvc(userId, followId);
-        const data = response ? response : "NOT_FOUND";
+        const data = response || "NOT_FOUND";
+        res.status(200);
+        res.send(data);
     } catch (e) {
         handleHttp(res, "ERROR_DELETE_FOLLOW");
     }
--- src/services/follows.services.ts
+++ src/services/follows.services.ts
@@ -1,55 +1,74 @@
 import UserModel from "@models/user.model";
 import { User } from "@interfaces/user.interface";
 
-const insertFollowSvc = async (userId: string, followId: string) => {
-  //Hay que insertar primero en la lista de following del userId  y luego en la lista de followers del followId
 
-  const responseInsert = await UserModel.updateOne(
-    { _id: userId },
-    { $push: { following: {user: followId,date: new Date()}}}
-  );
+const isFollowingUser = async (userId: string, followId: string): Promise<boolean> => {
+  try {
+    // Buscar al usuario seguidor
+    const user = await UserModel.findById(userId);
 
-  const responseInsert2 = await UserModel.updateOne(
-    { _id: followId },
-    { $push: { followers: {user: userId,date: new Date()}}}
-  );
+    // Verificar si el usuario seguido ya está en la lista de following del usuario seguidor
+    const isFollowing = user ? user.following.some(follow => follow.user.toString() === followId) : false;
 
-  //Devolver el resultado de ambas inserciones
-  return responseInsert && responseInsert2;
-
-
-}
-
-
-
-// const insertFollowSvc = async (userId: string, followId: string) => {
-//   const responseInsert = await UserModel.updateOne(
-//     { _id: userId },
-//     { $push: { followers: {user: followId,date: new Date()}}}
-//   );
-//   return responseInsert;
-// };
-
-const deleteFollowSvc = async (userId: string, followId: string) => {
-
-  const responseDelete = await UserModel.updateOne(
-    { _id: userId },
-    { $pull: { following: {user: followId}}}
-  );
-
-  const responseDelete2 = await UserModel.updateOne(
-    { _id: followId },
-    { $pull: { followers: {user: userId}}}
-  );
-
-  //Devolver el resultado de ambas inserciones
-  return responseDelete && responseDelete2;
-
-  // const responseItem = await UserModel.updateOne(
-  //   { _id: followId },
-  //   { $pull: { followers: { user: followId } } }
-  // );
-  // return responseItem;
+    return isFollowing;
+  } catch (error) {
+    console.error(`Error al verificar el seguimiento: ${error}`);
+    return false; // Devolver false cuando se produce una excepción
+  }
 };
 
-export { insertFollowSvc, deleteFollowSvc};
+
+const insertFollowSvc = async (userId: string, followId: string) => {
+  try {
+    console.log(`Insertando seguimiento de ${userId} a ${followId}`);
+
+    // Primero, insertar en la lista de following del userId
+    const responseInsert1 = await UserModel.updateOne(
+      { _id: userId },
+      { $push: { following: { user: followId, date: new Date() } } }
+    );
+
+    console.log(`Resultado de la primera inserción: ${JSON.stringify(responseInsert1)}`);
+
+    // Luego, insertar en la lista de followers del followId
+    const responseInsert2 = await UserModel.updateOne(
+      { _id: followId },
+      { $push: { followers: { user: userId, date: new Date() } } }
+    );
+
+    console.log(`Resultado de la segunda inserción: ${JSON.stringify(responseInsert2)}`);
+
+
+    // Devolver el resultado de ambas inserciones
+    return responseInsert1 && responseInsert2;
+  } catch (error) {
+    console.error(`Error al insertar el seguimiento: ${error}`);
+    throw error;
+  }
+};
+
+
+const deleteFollowSvc = async (userId: string, followId: string): Promise<boolean> => {
+  try {
+    // Eliminar followId de la lista de following del usuario
+    const responseDelete = await UserModel.updateOne(
+      { _id: userId },
+      { $pull: { following: {user: followId}}}
+    );
+
+    // Eliminar userId de la lista de followers de followId
+    const responseDelete2 = await UserModel.updateOne(
+      { _id: followId },
+      { $pull: { followers: {user: userId}}}
+    );
+
+    // Devolver true si ambas operaciones fueron exitosas
+    return responseDelete.modifiedCount > 0 && responseDelete2.modifiedCount > 0;
+  } catch (error) {
+    console.error(`Error al eliminar el seguimiento: ${error}`);
+    throw error;
+  }
+};
+
+export { insertFollowSvc, deleteFollowSvc, isFollowingUser};
+
